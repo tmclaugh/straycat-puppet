3bbc639c274a063621f8b7a2e3b0db87f9e1c2a1
Tom McLaughlin <tmclaugh@sdf.lonestar.org>
2014-12-13 20:24:30 -0500

    make passenger reusable.

---
+++ modules/straycat/manifests/svc/passanger.pp
@@ -0,0 +1,40 @@
+# == Class: class_name
+#
+# Short description of class.
+#
+# === Parameters
+#
+# [*parameter*]
+#   Description of parameter and its usage.
+#
+# === Examples
+#
+#   class { 'class_name':
+#     parameter => 'value'
+#   }
+#
+# === Authors
+#
+# tmclaughlin@hubspot.com
+#
+# === Copyright
+#
+# Copyright 2014 Hubspot
+#
+class straycat::svc::passenger {
+
+  $passenger_version = '4.0.48'
+
+  class { '::passenger':
+    package_ensure         => $passenger_version,
+    passenger_version      => $passenger_version,
+    package_provider       => 'gem',
+    compile_passenger      => true,
+    mod_passenger_location => "/usr/lib/ruby/gems/1.8/gems/passenger-${passenger_version}/buildout/apache2/mod_passenger.so",
+    passenger_root         => "/usr/lib/ruby/gems/1.8/gems/passenger-${passenger_version}/",
+    passenger_poolsize     => $::processorcount * 1.5,
+    require                => Package['gcc-c++']
+  }
+  contain '::passenger'
+
+}
--- modules/straycat/manifests/svc/puppet/master.pp
+++ modules/straycat/manifests/svc/puppet/master.pp
@@ -44,21 +44,11 @@ class straycat::svc::puppet::master (
 
   # Passenger related.
   #
-  $passenger_version = '4.0.48'
   ensure_resource('package', 'gcc-c++')
 
 
-  class { '::passenger':
-    package_ensure         => $passenger_version,
-    passenger_version      => $passenger_version,
-    package_provider       => 'gem',
-    compile_passenger      => true,
-    mod_passenger_location => "/usr/lib/ruby/gems/1.8/gems/passenger-${passenger_version}/buildout/apache2/mod_passenger.so",
-    passenger_root         => "/usr/lib/ruby/gems/1.8/gems/passenger-${passenger_version}/",
-    passenger_poolsize     => $::processorcount * 1.5,
-    require                => Package['gcc-c++']
-  }
-  contain '::passenger'
+  class { '::straycat::svc::passenger': }
+  contain '::straycat::svc::passenger'
 
   # Hiera related
   ensure_resource('package', 'ruby-devel')
