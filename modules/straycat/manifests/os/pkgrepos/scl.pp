998b24b9ed86261803e6bb73cf6e5cf5cf96f475
Tom McLaughlin <tmclaugh@sdf.lonestar.org>
2014-12-14 19:48:07 -0500

    Add SCL yum repo.

---
+++ modules/straycat/manifests/os/pkgrepos/scl.pp
@@ -0,0 +1,49 @@
+# == Class: straycat::os::pkgrepos::scl
+#
+# Add RedHat SCL repo.
+#
+# === Parameters
+#
+# [*enabled*]
+#   If the repo should be enabled.
+#
+# === Examples
+#
+#   class { '::straycat::os::pkgrepos::scl': }
+#
+# === Authors
+#
+# tmclaugh@sdf.lonestar.org
+#
+# === Copyright
+#
+# Copyright 2014 Tom McLaughlin
+#
+class straycat::os::pkgrepos::scl (
+  $enabled = true
+) {
+
+  if $enabled == true {
+    $repo_enabled = '1'
+  } else {
+    $repo_enabled = '0'
+  }
+
+  anchor { 'straycat::os::pkgrepos::scl::start': }
+
+  yumrepo { 'scl':
+    descr           => 'CentOS-\$releasever - SCL',
+    baseurl         => 'http://mirror.centos.org/centos/$releasever/SCL/$basearch/',
+    enabled         => $repo_enabled,
+    gpgcheck        => '1',
+    gpgkey          => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-${::centos_major}",
+    notify          => straycat::os::pkgrepos::Repo_clean['scl']
+  }
+
+  straycat::os::pkgrepos::repo_clean{ 'scl': }
+
+  anchor { 'straycat::os::pkgrepos::scl::end':
+    require => straycat::os::pkgrepos::Repo_clean['scl'],
+  }
+
+}
