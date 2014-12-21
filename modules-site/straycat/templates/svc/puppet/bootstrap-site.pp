923349489b83eb5e355d12b0dc97d548f62313bd
Tom McLaughlin <tmclaugh@sdf.lonestar.org>
2014-12-21 02:51:18 -0500

    add our site.pp for bootstrap role.

---
+++ modules-site/straycat/templates/svc/puppet/bootstrap-site.pp
@@ -0,0 +1,11 @@
+# apply requested role or use our default.  This is not secure.  Only meant
+# to be used when setting up a new environment.
+
+node default {
+
+  if $::role {
+    class { $role: }
+  } else {
+    class { '::straycat::roles::base': }
+  }
+}
