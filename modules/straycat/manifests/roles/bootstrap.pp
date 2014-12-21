fae18ff9b9a6007f8feaae5863330472214aa6fe
Tom McLaughlin <tmclaugh@sdf.lonestar.org>
2014-12-20 23:56:02 -0500

    add a bootstrap role that can be used as a preliminary puppetmaster

---
+++ modules/straycat/manifests/roles/bootstrap.pp
@@ -0,0 +1,38 @@
+# == Class: straycat::roles::bootstrap
+#
+# Create a bootstrap puppetmaster.
+#
+# This will create a host to be a temporary puppetmaster. The host will be
+# used to configure basic services.  Once that is done and basics are in
+# place (ex. puppetmaster, puppetdb, foreman) then this host can be removed.
+#
+# === Parameters
+#
+# NONE
+#
+# === Examples
+#
+# This role should be applied to a host via an ENC.
+#
+# === Authors
+#
+# tmclaugh@sdf.lonestar.org
+#
+# === Copyright
+#
+# Copyright 2014 Tom McLaughlin
+#
+class straycat::roles::bootstrap {
+  include stdlib
+
+  class { '::straycat::os':
+    stage => setup
+  }
+
+  class { '::straycat::svc::puppet::master':
+    puppet_psk      => '',
+    enable_puppetdb => false,
+    enable_foreman  => false
+  }
+
+}
