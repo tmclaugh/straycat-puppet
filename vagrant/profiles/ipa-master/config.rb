80597eb0d3120f49b4f9f5aa7fdb5eb300bc11f3
Tom McLaughlin <tmclaugh@sdf.lonestar.org>
2014-12-22 14:13:33 -0500

    Setup and IPA master

---
+++ vagrant/profiles/ipa-master/config.rb
@@ -0,0 +1,22 @@
+# == Profile: ipa-master
+#
+# Create an IPA master host.
+#
+# ==== Client access.
+#
+# This profile will manipulate the /etc/hosts file on instances and the
+# local host so that name resolution will work.  The master host can be
+# accessed using the following URL:
+#
+# https://<instance_name>.straycat.local
+#
+Vagrant.configure('2') do |config|
+
+  # Manage entries for instances on local host.
+  config.hostmanager.manage_host = true
+
+  config.vm.define "ipa-master", primary: true do |ipa|
+    ipa.vm.network :private_network, ip: "192.168.3.10"
+  end
+
+end
