2c14120f7f68b6feea6061718199d2d62f29af49
Tom McLaughlin <tmclaugh@sdf.lonestar.org>
2014-12-22 13:42:53 -0500

    deploy puppet using r10k

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -5,7 +5,11 @@
 
 BS_DIR=$(dirname $0)
 
+# Setup puppetmaster
 puppet apply \
     --verbose \
     --modulepath ${BS_DIR}/../modules:${BS_DIR}/../modules-site \
     -e 'include ::straycat::roles::bootstrap'
+
+# deploy environment
+r10k deploy environment -p -v
