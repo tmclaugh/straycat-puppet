782853210d476265be7e4e79377a8115269d80f1
Tom McLaughlin <tmclaugh@sdf.lonestar.org>
2014-12-22 13:46:37 -0500

    add exmaple multi host profile

---
+++ vagrant/profiles/example-multi/config.rb
@@ -0,0 +1,14 @@
+# Example for provisioning multiple hosts.
+Vagrant.configure('2') do |config|
+  # primary: designates this will be the default host when no hostname is
+  # given to commands.
+  config.vm.define "node1", primary: true do |node1|
+    node1.vm.hostname = 'puppetmaster.straycat.local'
+    node1.vm.network "private_network", ip: "192.168.3.10"
+  end
+
+  config.vm.define "node2" do |node2|
+    node2.vm.hostname = 'client.straycat.local'
+    node2.vm.network "private_network", ip: "192.168.3.11"
+  end
+end
