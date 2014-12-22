52691253c759b3caefd936c41ef44b07c6bcbf3c
Tom McLaughlin <tmclaugh@sdf.lonestar.org>
2014-12-22 14:11:47 -0500

    add a profile for standing up a bootstrap puppetmaster using bootstrap/bootstrap.sh

---
+++ vagrant/profiles/puppet-bootstrap/config.rb
@@ -0,0 +1,27 @@
+Vagrant.configure('2') do |config|
+
+  config.hostmanager.manage_host = true
+
+  config.vm.define "puppetmaster" do |puppet|
+    puppet.vm.hostname = 'puppetmaster.straycat.local'
+    puppet.hostmanager.aliases = 'puppetmaster'
+    puppet.vm.network :private_network, ip: "192.168.4.11"
+
+    puppet.vm.provision :puppet, id: 'default_puppet' do |p|
+      p.manifests_path = PUPPET_MANIFEST_PATH
+      p.manifest_file  = "noop.pp"
+    end
+
+    puppet.vm.provision :puppet_server, id: 'default_puppet_server' do |p|
+    end
+
+    # FIXME: Can't find git host ffrom vagrant.  Requires adding to
+    # /etc/hosts manually.
+    #
+    # run bootstrap script.
+    #config.vm.provision :shell do |shell|
+    #  shell.inline = "/bin/sh /etc/puppet-local/bootstrap/bootstrap.sh"
+    #end
+  end
+
+end
