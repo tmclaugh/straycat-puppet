ec44f88433cb10d92be4be85e68debab5de41cef
Tom McLaughlin <tmclaugh@sdf.lonestar.org>
2014-12-21 21:10:27 -0500

    fix to work with latest passenger.  Maybe eventually get foreman class to use this too.

--- modules-site/straycat/manifests/svc/passenger.pp
+++ modules-site/straycat/manifests/svc/passenger.pp
@@ -27,8 +27,10 @@ class straycat::svc::passenger {
   $passenger_version = '4.0.18'
   $passenger_pkg_release = "${passenger_version}-9.6.${::centos_pkg_release}"
 
-  # We need 4.0.x for Foreman but SCL was a pain.  Just use the Foreman pkgs
-  class { '::straycat::os::pkgrepos::foreman': }
+  include '::straycat::os::pkgrepos::foreman'
+  contain '::straycat::os::pkgrepos::foreman'
+  include '::straycat::os::pkgrepos::scl'
+  contain '::straycat::os::pkgrepos::scl'
 
   class { '::passenger':
     package_name           => 'mod_passenger',
@@ -36,10 +38,11 @@ class straycat::svc::passenger {
     passenger_version      => $passenger_version, # Used only to distinguish v3 v. v4
     package_provider       => 'yum',
     mod_passenger_location => '/usr/lib64/httpd/modules/mod_passenger.so',
-    passenger_root         => "/usr/lib/ruby/gems/1.8/gems/passenger-${passenger_version}/",
-    passenger_poolsize     => $::processorcount * 1.5,
+    passenger_root         => "/usr/lib/ruby/gems/1.8/gems/passenger-${passenger_version}/lib/phusion_passenger/locations.ini",
     passenger_ruby         => '/usr/bin/ruby',
-    require                => Class['::straycat::os::pkgrepos::foreman']
+    passenger_poolsize     => $::processorcount * 1.5,
+    require                => [ Class['::straycat::os::pkgrepos::scl'],
+                                Class['::straycat::os::pkgrepos::foreman']],
   }
 
   contain '::passenger'
