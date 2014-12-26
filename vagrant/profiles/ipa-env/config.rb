3e0e4a823f6322aafd8274bbe81f9d4262a41301
Tom McLaughlin <tmclaugh@sdf.lonestar.org>
2014-12-26 03:05:48 -0500

    Add a master and client role for testing.

---
+++ vagrant/profiles/ipa-env/config.rb
@@ -0,0 +1,24 @@
+Vagrant.configure('2') do |config|
+
+  config.hostmanager.manage_host = true
+
+  config.vm.define "ipa" do |ipa|
+    ipa.vm.hostname = 'ipa-master.straycat.local'
+    ipa.vm.network :private_network, ip: "192.168.4.10"
+
+    ipa.vm.provision :puppet_server, id: 'default_puppet_server' do |puppet|
+      puppet.options       = "--verbose --waitforcert 120"
+      puppet.facter        = { 'role' => 'straycat::roles::dc::master'}
+    end
+  end
+
+  config.vm.define "base" do |base|
+    base.vm.hostname = 'base.straycat.local'
+    base.vm.network :private_network, ip: "192.168.4.11"
+
+    base.vm.provision :puppet_server, id: 'default_puppet_server' do |puppet|
+      puppet.options       = "--verbose --waitforcert 120"
+      puppet.facter        = { 'role' => 'straycat::roles::base'}
+    end
+  end
+end
