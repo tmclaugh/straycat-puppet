977394cc0aee9a6a69f6c39fb1405e70309daee6
Tom McLaughlin <tmclaugh@sdf.lonestar.org>
2014-12-27 02:01:25 -0500

    Add doc.

--- modules-site/straycat/manifests/roles/foreman.pp
+++ modules-site/straycat/manifests/roles/foreman.pp
@@ -1,21 +1,14 @@
-# == Class: class_name
+# == Class: straycat::roles::foreman
 #
-# Short description of class.
-#
-# === Parameters
-#
-# [*parameter*]
-#   Description of parameter and its usage.
+# Create a foreman host.
 #
 # === Examples
 #
-#   class { 'class_name':
-#     parameter => 'value'
-#   }
+# This should be attached to a host via an ENC.
 #
 # === Authors
 #
-# tmclaugh@sdf.lonestar.org
+# Tom McLaughlin <tmclaugh@sdf.lonestar.org>
 #
 # === Copyright
 #
