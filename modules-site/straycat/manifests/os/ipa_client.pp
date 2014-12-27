1a0db8b337076a0710100d5ca3c8990769493a66
Tom McLaughlin <tmclaugh@sdf.lonestar.org>
2014-12-26 19:33:06 -0500

    Move IPA setup to Hiera

--- modules-site/straycat/manifests/os/ipa_client.pp
+++ modules-site/straycat/manifests/os/ipa_client.pp
@@ -31,16 +31,29 @@
 #
 # Copyright 2014 Hubspot
 #
-class straycat::os::ipa_client {
+class straycat::os::ipa_client (
+  $ipa_domain    = undef,
+  $ipa_join_user = undef,
+  $ipa_join_pass = undef,
+  $ipa_realm     = undef,
+  $ipa_server    = undef,
+) {
 
-  class { '::ipa':
-    domain      => 'straycat.local',
-    realm       => 'STRAYCAT.LOCAL',
-    otp         => 'YouDownWithOTP',  # FIXME: Need to better understand this.
-    client      => true,
-    automount   => false,
-    sudo        => false,
-    mkhomedir   => false,
-    loadbalance => false
-  }
+  # FIXME: I don't like how this class works so do it on my own below.
+  #class { '::ipa':
+  #  domain      => 'straycat.local',
+  #  realm       => 'STRAYCAT.LOCAL',
+  #  otp         => 'YouDownWithOTP',  # FIXME: Need to better understand this.
+  #  client      => true,
+  #  automount   => false,
+  #  sudo        => false,
+  #  mkhomedir   => false,
+  #  loadbalance => false
+  #}
+
+    exec { 'ipa-join':
+      command => "ipa-client-install -U --enable-dns-updates --no-krb5-offline-passwords --domain=${ipa_domain} --realm=${ipa_realm} -p ${ipa_join_user} -w ${ipa_join_pass} -N --server=${ipa_server}",
+      path    => ['/usr/sbin'],
+      creates => '/etc/ipa/ca.crt'
+    }
 }
