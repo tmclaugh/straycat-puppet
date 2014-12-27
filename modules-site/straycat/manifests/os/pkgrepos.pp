b20a260bae3e102cbdef1c34c457d1bf0d97af97
Tom McLaughlin <tmclaugh@sdf.lonestar.org>
2014-12-26 22:40:16 -0500

    Hookup centos repos accidentally added in previous commit.

--- modules-site/straycat/manifests/os/pkgrepos.pp
+++ modules-site/straycat/manifests/os/pkgrepos.pp
@@ -16,7 +16,13 @@
 #
 class straycat::os::pkgrepos {
 
+  class { '::straycat::os::pkgrepos::centos': }
+  contain '::straycat::os::pkgrepos::centos'
+
   class { '::straycat::os::pkgrepos::epel': }
   contain '::straycat::os::pkgrepos::epel'
+
+  Class['::straycat::os::pkgrepos::centos'] ->
+  Class['::straycat::os::pkgrepos::epel']
 
 }
