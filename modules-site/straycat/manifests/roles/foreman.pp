54f3be52cd53f63b4891811010b2a6d3ff589f19
Tom McLaughlin <tmclaugh@sdf.lonestar.org>
2015-01-06 13:51:45 -0500

    Move foreman setup to Hiera

--- modules-site/straycat/manifests/roles/foreman.pp
+++ modules-site/straycat/manifests/roles/foreman.pp
@@ -18,23 +18,10 @@ class straycat::roles::foreman {
 
   include stdlib
 
-  $foreman_admin_username = 'admin'
-  $foreman_admin_password = 'changeme'
-
-  $foreman_db_host     = 'pgsql.straycat.local'
-  $foreman_db_username = 'foreman'
-  $foreman_db_password = 'MyWeakPassword' # Move to Hiera when in place
-
   class { 'straycat::os':
     stage => setup
   }
 
-  class { '::straycat::svc::puppet::foreman':
-    foreman_admin_username => $foreman_admin_username,
-    foreman_admin_password => $foreman_admin_password,
-    foreman_db_host        => $foreman_db_host,
-    foreman_db_username    => $foreman_db_username,
-    foreman_db_password    => $foreman_db_password
-  }
+  class { '::straycat::svc::puppet::foreman': }
 
 }
