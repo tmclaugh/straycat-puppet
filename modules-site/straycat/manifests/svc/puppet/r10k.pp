eaed58da9a23e0681ea6bbb4f4009c63d1378cf1
Tom McLaughlin <tmclaugh@sdf.lonestar.org>
2015-01-07 09:36:22 -0500

    Use public repo for Puppet.

--- modules-site/straycat/manifests/svc/puppet/r10k.pp
+++ modules-site/straycat/manifests/svc/puppet/r10k.pp
@@ -13,6 +13,10 @@
 #     parameter => 'value'
 #   }
 #
+# === FIXME
+#
+# * Move config to Hiera.
+#
 # === Authors
 #
 # tmclaugh@sdf.lonestar.org
@@ -24,27 +28,8 @@
 class straycat::svc::puppet::r10k {
 
   # FIXME: move to hiera
-  $version           = '1.4.0'
-  $git_host          = 'git.straycat.dhs.org'
-  $git_url           = "gitolite@${git_host}:puppet.git"
-  $git_host_key_type = 'ssh-rsa'
-  $git_host_key      = 'AAAAB3NzaC1yc2EAAAABIwAAAQEAvn2el8mkkx8fKK4tnswlISXa7bLRaxn/rAXigP9Cvls0q9LARUBws1XTCbIz8NpXjuG5u7x4KMANyg0O7ja+CjdB+SBt3wk27VPtgF8D70K9Xh9e8/LjeoY3ALpEYMNwrNX8CjARz5J2V8caSj3479IB/ruHUC6J0LEZgLH4zEV87pTle8mL2c3iG4TR1Lc1wstu41ZVjed0xzXjnfI85No8h1xLxgnuz68nrfkb+SgzkPOFGLv4T/oFfzr5R0rbewHHLMxKHxtgWUFr4JdibJdfMHQSu0g0TbyjR5PmhOpmBsPS2OozorYAkUtYGhrDtr1B02dn82J4d/AGWLP24Q=='
-
-  file { '/root/.ssh/':
-    ensure  => directory,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0600',
-  }
-
-  sshkey { $git_host:
-    ensure  => 'present',
-    target  => '/root/.ssh/known_hosts',
-    type    => $git_host_key_type,
-    key     => $git_host_key,
-    require => File['/root/.ssh/']
-  }
-
+  $version = '1.4.0'
+  $git_url = 'https://github.com/tmclaugh/straycat-puppet.git'
   class { '::r10k':
     version      => $version,
     remote       => $git_url,
@@ -52,6 +37,5 @@ class straycat::svc::puppet::r10k {
     provider     => 'gem'
   }
   contain '::r10k'
-
 
 }
