ef05ed634f63c1ba233d7a7b65372feef5850c45
Tom McLaughlin <tmclaugh@sdf.lonestar.org>
2015-01-08 23:30:04 -0500

    disable node importing now that foreman will work.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,6 @@
 import "exec.pp"
-import "nodes/*.pp"
+# Disable because we have Foreman
+#import "nodes/*.pp"
 
 # Just make stdlib available for use.  This also establishes our stages
 # as well.  See stdlib::stages.
