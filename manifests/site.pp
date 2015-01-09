f1d2b7d051049d78ba62b984a46ddfcfd518e7e0
Tom McLaughlin <tmclaugh@sdf.lonestar.org>
2015-01-08 23:30:49 -0500

    set Exec defaults globally.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,3 @@
-import "exec.pp"
 # Disable because we have Foreman
 #import "nodes/*.pp"
 
@@ -6,3 +5,12 @@ import "exec.pp"
 # as well.  See stdlib::stages.
 include stdlib
 
+$exec_path = $::operatingsystem ? {
+  centos => ["/bin", "/sbin", "/usr/bin", "/usr/sbin", "/usr/local/bin", "/usr/local/sbin", "/usr/kerberos/sbin", "/usr/kerberos/bin", "/usr/X11R6/bin", "/usr/X11R6/sbin"],
+  default => ["/bin", "/sbin", "/usr/bin", "/usr/sbin"],
+}
+
+Exec {
+  path => $exec_path,
+  logoutput => true
+}
