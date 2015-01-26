8be1be9af0a3943c60028b8915e758b3b99690df
Tom McLaughlin <tmclaugh@sdf.lonestar.org>
2015-01-26 10:05:08 -0500

    Fix hostnames to be more generic.

--- vagrant/profiles/example-multi/config.rb
+++ vagrant/profiles/example-multi/config.rb
@@ -3,12 +3,12 @@ Vagrant.configure('2') do |config|
   # primary: designates this will be the default host when no hostname is
   # given to commands.
   config.vm.define "node1", primary: true do |node1|
-    node1.vm.hostname = 'puppetmaster.straycat.local'
+    node1.vm.hostname = 'node1.straycat.local'
     node1.vm.network "private_network", ip: "192.168.3.10"
   end
 
   config.vm.define "node2" do |node2|
-    node2.vm.hostname = 'client.straycat.local'
+    node2.vm.hostname = 'node2.straycat.local'
     node2.vm.network "private_network", ip: "192.168.3.11"
   end
 end
