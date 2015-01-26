00b19e62e0a3e8f836e60ae9bf1aded694b92996
Tom McLaughlin <tmclaugh@sdf.lonestar.org>
2015-01-26 13:29:12 -0500

    add a noop role.  It was useful for testing.

---
+++ modules-site/straycat/manifests/roles/noop.pp
@@ -0,0 +1,16 @@
+# == Class: straycat::roles::noop
+#
+# For when you want nothing to happen.
+#
+# === Authors
+#
+# tmclaugh@sdf.lonestar.org
+#
+# === Copyright
+#
+# Copyright 2014 Tom McLaughlin
+#
+class straycat::roles::noop {
+
+
+}
