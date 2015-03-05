8634534d06eda93ea333fbd878f8cc56d729a51f
Tom McLaughlin <tom@jana.com>
2015-03-05 17:45:19 -0500

    Setup basic debian Apt sources

--- modules-site/jana/manifests/os/pkgrepos.pp
+++ modules-site/jana/manifests/os/pkgrepos.pp
@@ -16,13 +16,39 @@
 #
 class jana::os::pkgrepos {
 
-  class { '::jana::os::pkgrepos::centos': }
-  contain '::jana::os::pkgrepos::centos'
+  if $::operatingsystem == 'centos' {
+    class { '::jana::os::pkgrepos::centos': }
+    contain '::jana::os::pkgrepos::centos'
 
-  class { '::jana::os::pkgrepos::epel': }
-  contain '::jana::os::pkgrepos::epel'
+    class { '::jana::os::pkgrepos::epel': }
+    contain '::jana::os::pkgrepos::epel'
 
-  Class['::jana::os::pkgrepos::centos'] ->
-  Class['::jana::os::pkgrepos::epel']
+    Class['::jana::os::pkgrepos::centos'] ->
+    Class['::jana::os::pkgrepos::epel']
+  } elsif $::operatingsystem == 'debian' {
 
+    class { '::apt':
+      always_apt_update    => false,
+      apt_update_frequency => undef,
+      disable_keys         => undef,
+      proxy_host           => false,
+      proxy_port           => '8080',
+      purge_sources_list   => true,
+      purge_sources_list_d => true,
+      purge_preferences_d  => true,
+      update_timeout       => undef,
+      fancy_progress       => true
+    }
+
+    class { '::apt::release':
+      release_id => $::lsbdistcodename,
+    }
+
+    class { '::jana::os::pkgrepos::debian': }
+
+    Class['::apt'] ->
+     Class['apt::release'] ->
+      Class['::jana::os::pkgrepos::debian']
+  }
 }
+
