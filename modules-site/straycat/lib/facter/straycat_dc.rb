e4fdbe2c23812922747ea3cdbff403ec43985ce4
Tom McLaughlin <tmclaugh@sdf.lonestar.org>
2015-11-14 14:08:18 -0500

    change how we handle setting straycat_dc when in AWS

--- modules-site/straycat/lib/facter/straycat_dc.rb
+++ modules-site/straycat/lib/facter/straycat_dc.rb
@@ -8,10 +8,10 @@ Facter.add('straycat_dc') do
   setcode do
     if Facter.value('domain') == 'straycat.dev'
       'local'
+    elsif Facter.value('ec2_placement_availability_zone') 
+      acter.value('ec2_placement_availability_zone')[0..-2]
     elsif Facter.value('domain') == 'straycat-net.lan'
       'home'
-    elsif Facter.value('domain') =~ /.*\.straycat-net.dhs.org$/
-      Facter.value('domain').split('.')[0]
     end
   end
 end
