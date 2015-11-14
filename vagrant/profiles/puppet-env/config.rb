c301cfed4c66b1419010f75b3fd63af2c0730713
Tom McLaughlin <tmclaugh@sdf.lonestar.org>
2015-11-14 15:46:49 -0500

    Introduce straycat_{env,svc} into Vagrant

--- vagrant/profiles/puppet-env/config.rb
+++ vagrant/profiles/puppet-env/config.rb
@@ -19,6 +19,8 @@ Vagrant.configure('2') do |config|
         "hiera_ver"              => $hiera_ver,
         "hiera_eyaml_ver"        => $hiera_eyaml_ver,
         "hiera_eyaml_gpg_ver"    => $hiera_eyaml_gpg_ver,
+        "straycat_env"           => 'dev',
+        "straycat_dc"            => 'local'
       }
     end
 
