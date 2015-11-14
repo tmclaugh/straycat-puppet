1b268abefdf0d841fff7ae3c141ec4365ecb1349
Tom McLaughlin <tmclaugh@sdf.lonestar.org>
2015-11-14 12:43:31 -0500

    Make straycat::os not something that needs to be realized in setup stage.
    We'll let the class itself determine what needs to be in setup and what
    can occur in main.  This is useful for things like monitoring which will
    have resources, ex. checks, that are realized in main.

--- modules-site/straycat/manifests/os.pp
+++ modules-site/straycat/manifests/os.pp
@@ -38,41 +38,21 @@ class straycat::os (
   $puppet_setup = true
 ) {
 
-  $real_ipa_setup = pick($::sc_ipa_setup, $ipa_setup)
-
   include stdlib
-  include concat::setup
 
   anchor { 'straycat::os::start': }
 
-  class { '::straycat::os::resolv': }
-
-  # repositories
-  class { '::straycat::os::pkgrepos': }
-
-  class { '::straycat::os::time': }
-
-  if $puppet_setup {
-    class { '::straycat::os::puppet':
-      require => Class['::straycat::os::time']
-    }
-    contain '::straycat::os::puppet'
-  }
-
-  if $real_ipa_setup {
-    class { '::straycat::os::ipa_client':
-      require => Class['::straycat::os::time']
-    }
-    contain '::straycat::os::ipa_client'
+  class { '::straycat::os::setup':
+    ipa_setup    => $ipa_setup,
+    puppet_setup => $puppet_setup,
+    stage        => setup
   }
 
   anchor { 'straycat::os::end': }
 
 
   Anchor['straycat::os::start'] ->
-  Class['::straycat::os::resolv'] ->
-  Class['::straycat::os::pkgrepos'] ->
-  Class['::straycat::os::time'] ->
+  Class['::straycat::os::setup'] ->
   Anchor['straycat::os::end']
 
 }
