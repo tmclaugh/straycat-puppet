89509e2d15b33462b6180589338812f32426c09e
Tom McLaughlin <tmclaugh@sdf.lonestar.org>
2015-11-15 16:24:16 -0500

    colorify prompt when in prod

--- modules-site/straycat/templates/os/user/straycat_prompt.sh
+++ modules-site/straycat/templates/os/user/straycat_prompt.sh
@@ -2,6 +2,20 @@
 
 # Set our custom prompt
 . /etc/default/straycat
+
+if [ "${STRAYCAT_ENV}" = 'prod' ]; then
+    if [ $(id -u) = '0' ]; then
+        color_start='\e[0;31m'
+        color_stop='\e[m'
+    else
+        color_start='\e[0;91m'
+        color_stop='\e[m'
+    fi
+else
+    color_start=''
+    color_stop=''
+fi
+
 if [ "$PS1" ]; then
-    PS1="[\u@\h ${STRAYCAT_SVC}:${STRAYCAT_ENV} \W]\\$"
+    PS1="${color_start}[\u@\h ${STRAYCAT_SVC}:${STRAYCAT_ENV} \W]\\$ ${color_stop}"
 fi
